0a4d3965781a584add471940831b9ffd817ec44d
rajendar talatam <rajendartalatam@rajendars-MacBook-Pro.local>
2019-06-30 19:31:38 +0530

    simple node app with test 3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! rajendar jagan')
+  response.send('Hello World! rajendar ====')
 })
 
 app.listen(app.get('port'), function() {
